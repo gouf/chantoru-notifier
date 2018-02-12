79882f6aa65bcafca8cd5bb18f99684d90155114
gouf <innocent.zero@gmail.com>
2018-02-12 09:42:02 +0900

    Remove unused codes

--- lib/chantoru/scraper.rb
+++ lib/chantoru/scraper.rb
@@ -7,7 +7,6 @@ module Chantoru
       @email    = ENV['CHANTORU_ID']
       @password = ENV['CHANTORU_PASS']
       @watir = ::Watir::Browser.new(:firefox)
-      @logger = Logger.new('log', 'daily')
       login
 
       # Can't break through Google's reCAPTCHA (the type for clicking image what specific object included).
@@ -31,10 +30,6 @@ module Chantoru
       end
     end
 
-    def info(text)
-      @logger.info(text)
-    end
-
     private
 
     def login
@@ -47,12 +42,6 @@ module Chantoru
 
       @watir.button(id: 'signInButton')
         .click
-      # @a.get(LOGIN_URL) do |page|
-      #   page.form_with(id: 'signInForm') do |f|
-      #     f.field_with(name: 'j_username').value = @email
-      #     f.field_with(name: 'j_password').value = @password
-      #   end.submit
-      # end
     end
 
     def latest_records
@@ -60,55 +49,7 @@ module Chantoru
       @watir.li(class: 'rawdata').click
       html = @watir.html
       json = Nokogiri::HTML(html).css('.data').first
-      pp JSON.load(json)['list'].map { |x| x['title'] }
-      # Get latest titles.
-      # new_titles = load_new_titles(page)
-      # old_titles = load_old_titles
-      # save_to_file(new_titles)
-      # extract_new_titles(new_titles, old_titles)
-    end
-
-    def load_new_titles(page)
-      return [] if JSON.restore(page.content.to_s)['list'].nil?
-      JSON.restore(page.content.to_s)['list'].map do |d|
-        "#{d['title']}"
-      end
-    end
-
-    def save_to_file(titles)
-      File.open('recorded_list.json', 'w') do |f|
-        f.write titles.to_json
-      end
-    end
-
-    def load_old_titles
-      JSON.restore(File.open('recorded_list.json', 'r').read)
-    end
-
-    def extract_new_titles(new_titles, old_titles)
-      # Compare 2 arrays.
-      # it will only get new titles
-
-      (new_titles | old_titles) - old_titles
-    end
-
-    def formated_body(body, type)
-      case type
-      when :text
-        body.inject('') {|formated_body, title|
-          formated_body += "・#{title}\n"
-        }
-      when :html
-        body.inject('') {|formated_body, title|
-          formated_body += "・#{title}<br />"
-        }
-      end
-    end
-
-    def write_current_pid
-      File.open('chantoru-notifier.pid', 'w') do |f|
-        f.write Process.pid
-      end
+      JSON.load(json)['list'].map { |x| x['title'] }
     end
   end
 end
